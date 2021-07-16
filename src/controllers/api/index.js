ba1ac940a7bb249397b4653645d6f3c8f0794785
Eti Priya <etipriya@gmail.com>
2021-07-16 23:12:53 +0100

    added getworkout function

---
+++ src/controllers/api/index.js
@@ -0,0 +1,26 @@
+const Workout = require("../../models/Workout");
+
+//Added get workouts function
+const getWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ]);
+    res.json(workouts);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+module.exports = {
+  getWorkouts,
+  addExercise,
+  createWorkout,
+  getWorkoutsInRange,
+};
