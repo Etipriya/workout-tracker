c8bdb81ef053a7af93314029928626465828581b
Eti Priya <etipriya@gmail.com>
2021-07-16 19:33:40 +0100

    added api routes

---
+++ src/routes/api/index.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const {
+  getWorkouts,
+  addExercise,
+  createWorkout,
+  getWorkoutsInRange,
+} = require("../../controllers/api/index");
+const router = Router();
+
+router.get("/api/workouts", getWorkouts);
+
+router.put("/api/workouts/:id", addExercise);
+
+router.post("/api/workouts", createWorkout);
+
+router.get("/api/workouts/range", getWorkoutsInRange);
+
+module.exports = router;
